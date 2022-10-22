d7c46ca6ddb574926e2c955c4c5593ebfa7f375c
sunskymen <1505024764@qq.com>
2022-10-22 22:18:08 +0800

    初始化页面部分内容

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="index">
+    <div class="left">
+      <div class="top">
+        <div class="text">
+          <div><a>个人博客</a></div>
+          <div>Theme for Hexo</div>
+        </div>
+        <div class="nav clearfix">
+          <ul class="menu">
+            <li class="menu-item">
+              <a><i class="iconfont icon-kehuleixing"></i>个人中心</a>
+            </li>
+            <li class="menu-item">
+              <a><i class="iconfont icon-Bars"></i>文章</a>
+            </li>
+            <li class="menu-item">
+              <a><i class="iconfont icon-Search"></i>搜索</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="floor">
+        <router-view></router-view>
+      </div>
+    </div>
+    <div class="right">
+      <router-view></router-view>
+    </div>
+    <RollingProgress></RollingProgress>
+  </div>
+</template>
+
+<script>
+import RollingProgress from '@/components/RollingProgress.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    RollingProgress
+  }
+}
+</script>
+
+<style lang="less">
+  .index {
+    display: flex;
+    width: 1200px;
+    margin: 0 auto;
+    .left {
+      width: 230px;
+      .top {
+        width: 230px;
+        height: 300px;
+        box-shadow: 1px 1px 5px rgb(212, 212, 212);
+        .text {
+          height: 110px;
+          padding-top: 10px;
+          background-color: #222;
+          div {
+            height: 40px;
+            line-height: 50px;
+            text-align: center;
+            font-size: 16px;
+            color: #ddd;
+          }
+          div:nth-child(1) {
+            color: #fff;
+            font-size: 25px;
+          }
+        }
+        .nav {
+          height: 200px;
+          // background-color: pink;
+          .menu {
+            height: 100px;
+            margin: 40px 0;
+            .menu-item {
+              height: 40px;
+              line-height: 40px;
+              padding-left: 20px;
+              color: #222;
+              a i {
+                margin-right: 10px;
+              }
+            }
+            .menu-item:hover {
+              background-color: rgb(237, 234, 234);
+              cursor: pointer;
+            }
+          }
+        }
+      }
+      .floor {
+        position: sticky;
+        top: 10px;
+        height: 300px;
+        width: 230px;
+        box-shadow: 1px 1px 5px rgb(212, 212, 212);
+        margin-top: 10px;
+        background-color: pink;
+      }
+    }
+    .right {
+      flex: 1;
+      margin-left: 10px;
+      height: 1000px;
+      background-color: aqua;
+    }
+  }
+</style>
---
+++ src/components/RollingProgress.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="scroll">
+    <i class="iconfont icon-xiangshangjiantoucuxiao"></i>
+    <span>{{ percent }} %</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RollingProgress',
+  data () {
+    return {
+      percent: 0
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll) // 监听页面滚动
+  },
+  methods: {
+    // 获取页面滚动距离
+    handleScroll (e) {
+      setTimeout(() => {
+        const scrollTop = window.pageYOffset
+        this.percent = Math.ceil((scrollTop / 581) * 100)
+        console.log(this.percent)
+      }, 100)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.scroll {
+  display: flex;
+  position: fixed;
+  right: 10px;
+  bottom: 20px;
+  width: 60px;
+  height: 25px;
+  color: white;
+  font-size: 12px;
+  background-color: #888;
+  text-align: center;
+  line-height: 25px;
+  i {
+    margin-right: 8px;
+  }
+  span {
+    width: 100px;
+  }
+}
+</style>
